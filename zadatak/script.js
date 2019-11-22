0712d4e0274970f9ea4cd365dabf4ef131d541a9
davkat518 <sanja.andric518@gmail.com>
2019-11-22 20:05:25 +0100

    ubacila brojeve

---
+++ zadatak/script.js
@@ -0,0 +1,9 @@
+//funkcija koja vraca vrednost
+function racunaj(prviBroj,drugiBroj, treciBroj){
+    var trecibroj = 5;
+    var prviBroj = prompt("unesi prvi broj:");
+    var drugiBroj= prompt("unesi drugi broj:");
+    var racunaj = (prviBroj + drugiBroj)*treciBroj;
+  return racunaj;
+    alert(racunaj);
+}
