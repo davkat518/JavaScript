9d68efc53564181655b741baced92fbdc26aed2f
davkat518 <sanja.andric@vojvodina.sr.gov.yu>
2019-11-25 12:27:53 +0100

    domaci

---
+++ zadatak/zadatak-js/script.js
@@ -0,0 +1,39 @@
+function racunaj(prviBroj,drugiBroj){
+//    var prviBroj = prompt("unesi prvi broj:");
+//   var drugiBroj= prompt("unesi drugi broj:");
+//    var treciBroj= prompt("unesi treci broj:");
+treciBroj = 5;
+    var prvi= parseFloat(prviBroj);
+    var drugi= parseFloat (drugiBroj);
+    var treci= parseFloat (treciBroj);
+  var zbir = (prvi + drugi)* treci;
+  return zbir;
+}
+
+var rezultat = racunaj(4,6); 
+alert("Rezultat je "+rezultat);
+
+ function otvori() {
+     window.open("Reklama.html", "_blank", "width=320,height=250");
+
+    }
+
+function obrisi(){
+    localStorage.clear();
+}
+
+
+function ofarbajPozadinu(){
+    boja= prompt("Izaberi boju");
+
+    document.body.style.backgroundColor = boja;
+}
+
+function ispisi() {
+//    for (var i = 0; i < input.length; i++) {
+//                text =text + input[i] + ", ";
+//            }
+     var a= document.getElementsByTagName("input").value;
+    console.log(a);
+    }
+
